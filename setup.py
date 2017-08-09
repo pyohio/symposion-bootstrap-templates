62a4de4b4db7abdae82895cdc7da97ad92896f20
Christopher Neugebauer <chrisjrn@gmail.com>
2017-08-08 19:25:28 -0700

    boop

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="symposion-bootstrap-templates",
     author="Christopher Neugebauer",
     author_email="_@chrisjrn.com",
-    version=registrasion.__version__,
+    version=0.0.1,
     description="Default templates for Symposion",
     url="http://github.com/chrisjrn/symposion-bootstrap-templates/",
     packages=find_packages(),
