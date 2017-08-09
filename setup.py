6f8375a79d914bd956216a531eb19c5e47099131
Christopher Neugebauer <chrisjrn@gmail.com>
2017-08-08 19:24:59 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name="symposion-bootstrap-templates",
+    author="Christopher Neugebauer",
+    author_email="_@chrisjrn.com",
+    version=registrasion.__version__,
+    description="Default templates for Symposion",
+    url="http://github.com/chrisjrn/symposion-bootstrap-templates/",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT licence",
+    ),
+)
