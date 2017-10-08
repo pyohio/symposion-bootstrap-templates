f8b041e7d61085edc81b8b9d7447621466c6295f
Christopher Neugebauer <chrisjrn@gmail.com>
2017-10-08 10:34:40 -0700

    Updates review table format to include score

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="symposion-bootstrap-templates",
     author="Christopher Neugebauer",
     author_email="_@chrisjrn.com",
-    version="0.0.1-dev3",
+    version="0.0.1-dev4",
     description="Default templates for Symposion",
     url="http://github.com/chrisjrn/symposion-bootstrap-templates/",
     packages=find_packages(),
