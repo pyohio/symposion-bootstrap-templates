6eb1a43d9be2ba40d4a0f570c4a5fd067c5fe35d
Christopher Neugebauer <chrisjrn@gmail.com>
2017-10-15 19:03:28 -0700

    Version increment

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="symposion-bootstrap-templates",
     author="Christopher Neugebauer",
     author_email="_@chrisjrn.com",
-    version="0.0.1-dev6",
+    version="0.0.1-dev7",
     description="Default templates for Symposion",
     url="http://github.com/chrisjrn/symposion-bootstrap-templates/",
     packages=find_packages(),
