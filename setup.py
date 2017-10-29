1697da6c9dbb47b5584754ef2bc6a647e58f2950
Christopher Neugebauer <chrisjrn@gmail.com>
2017-10-29 16:35:04 -0700

    Debox

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="symposion-bootstrap-templates",
     author="Christopher Neugebauer",
     author_email="_@chrisjrn.com",
-    version="0.0.1-dev8",
+    version="0.0.1-dev9",
     description="Default templates for Symposion",
     url="http://github.com/chrisjrn/symposion-bootstrap-templates/",
     packages=find_packages(),
